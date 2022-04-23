4f4707f7267823de51c472fd188de9a04a821e03
Annie <a.janice.chen@gmail.com>
2022-04-23 22:40:47 +0200

    cards are responsive

--- js/app.js
+++ js/app.js
@@ -5,25 +5,53 @@ let projects = [
         name: "Food Magazine",
         image: "https://www.imore.com/sites/imore.com/files/styles/large/public/field/image/2020/05/acnh-best-villagers.jpg",
         description: "b",
-        url: "c"
+        url: 
+        { 
+            demo: "",
+            app: ""
+        }
     },
     {
         name: "Cattack to Attack",
-        image: "https://www.imore.com/sites/imore.com/files/styles/large/public/field/image/2020/05/acnh-best-villagers.jpg",
+        image: "./images/cattack.png",
         description: "b",
-        url: "c"
+        url: 
+        { 
+            demo: "",
+            app: "https://ajanicechen.github.io/Cattack-to-Protecc/"
+        }
+
     },
     {
         name: "HoloHome",
-        image: "https://www.imore.com/sites/imore.com/files/styles/large/public/field/image/2020/05/acnh-best-villagers.jpg",
+        image: "./images/holohome.png",
         description: "b",
-        url: "c"
+        url:     
+        { 
+            demo: "https://youtu.be/2OChiWkDt5U",
+            app: ""
+        }
+
     },
     {
         name: "Rest:API",
         image: "https://www.imore.com/sites/imore.com/files/styles/large/public/field/image/2020/05/acnh-best-villagers.jpg",
         description: "b",
-        url: "c"
+        url:    
+        { 
+            demo: "",
+            app: ""
+        }
+    },
+    {
+        name: "Style Transfer",
+        image: "./images/styleTransfer.png",
+        description: "b",
+        url:    
+        { 
+            demo: "https://youtu.be/ewtF3Dbkyvo",
+            app: "https://stud.hosted.hr.nl/0999525/PRG8-Week9/"
+        }
     }
 ]
 
@@ -36,11 +64,11 @@ let portfolioCardsContainer = document.getElementById('portfolioCardsContainer')
 let contentContainer = document.getElementById('contentContainer').children
 
 function createPortfolio(){
+
     for (let project of projects){
         //create cards
         let card = document.createElement('div')
         card.classList.add('card')
-        card.classList.add('portfolioCard')
 
         let cardBody = document.createElement('div')
         cardBody.classList.add('card-body')
@@ -53,15 +81,18 @@ function createPortfolio(){
         pImg.classList.add('card-img-top')
         pImg.src = project.image
 
+        // let pDescription = document.createElement('p')
+        // pDescription.innerText = project.description
+
         //append cards to parent
         portfolioCardsContainer.appendChild(card)
         card.appendChild(pImg)
         card.appendChild(cardBody)
         cardBody.appendChild(pName)
+        // cardBody.appendChild(pDescription)
         
     }
 }
-
 
 function changeContent(button_id){
     
